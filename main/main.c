95d2ffdcc0b484fdf10cdf1c337ec253224e9f75
n0p <lbernal@gmail.com>
2025-07-18 16:40:29 +0200

    initial hack

--- main/main.c
+++ main/main.c
@@ -1,14 +1,54 @@
+#include <stdio.h>
+#include "bsp/device.h"
+#include "bsp/display.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "hal/lcd_types.h"
+#include "nvs_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
 
 #include "rom/ets_sys.h"
 
 void dhrystone(void *pvParameters);
 void whetstone(void *pvParameters);
 
-void app_main()
-{
+// Constants
+static char const TAG[] = "main";
+
+// Global variables
+static esp_lcd_panel_handle_t       display_lcd_panel    = NULL;
+static esp_lcd_panel_io_handle_t    display_lcd_panel_io = NULL;
+static size_t                       display_h_res        = 0;
+static size_t                       display_v_res        = 0;
+static lcd_color_rgb_pixel_format_t display_color_format;
+
+void app_main(void) {
+    // Start the GPIO interrupt service
+    gpio_install_isr_service(0);
+
+    // Initialize the Non Volatile Storage service
+    esp_err_t res = nvs_flash_init();
+    if (res == ESP_ERR_NVS_NO_FREE_PAGES || res == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        res = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(res);
+
+    // Initialize the Board Support Package
+    ESP_ERROR_CHECK(bsp_device_initialize());
+
+    // Fetch the handle for using the screen, this works even when
+    res = bsp_display_get_panel(&display_lcd_panel);
+    ESP_ERROR_CHECK(res);                             // Check that the display handle has been initialized
+    bsp_display_get_panel_io(&display_lcd_panel_io);  // Do not check result of panel IO handle: not all types of
+                                                      // display expose a panel IO handle
+    res = bsp_display_get_parameters(&display_h_res, &display_v_res, &display_color_format);
+    ESP_ERROR_CHECK(res);  // Check that the display parameters have been initialized
+
+    ESP_LOGW(TAG, "Hello world!");
+
+
 	TaskHandle_t taskHandle = xTaskGetCurrentTaskHandle();
 	char * my_name = pcTaskGetName(NULL);
 	UBaseType_t my_prio = uxTaskPriorityGet(NULL);
@@ -30,6 +70,8 @@ void app_main()
 	printf("Target is ESP32C6@%"PRIu32"Mhz\n", ets_get_cpu_frequency());
 #elif CONFIG_IDF_TARGET_ESP32H2
 	printf("Target is ESP32H2@%"PRIu32"Mhz\n", ets_get_cpu_frequency());
+#elif CONFIG_IDF_TARGET_ESP32P4
+	printf("Target is ESP32P4@%"PRIu32"Mhz\n", ets_get_cpu_frequency());
 #endif
 
 	xTaskCreate(&dhrystone, "DHRY", 1024*4, (void *)taskHandle, my_prio+1, NULL);
@@ -39,4 +81,6 @@ void app_main()
 	xTaskCreate(&whetstone, "WHET", 1024*4, (void *)taskHandle, my_prio+1, NULL);
 	ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
 	ESP_LOGD(my_name, "ulTaskNotifyTake");
+
 }
+
